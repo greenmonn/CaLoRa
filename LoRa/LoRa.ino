0f5cca5f98fde70bfb6ad7e97e94da987be1f173
Mookeun Ji <goofcode@gmail.com>
2018-10-18 23:06:50 -0400

    Modify main ino file

--- LoRa/LoRa.ino
+++ LoRa/LoRa.ino
@@ -1,7 +1,8 @@
 #include "LoRaWan.h"
 
 #define SERIAL_BAUDRATE 9600
-#define RF_FREQUENCY 868.0
+
+LoRaWan* loRaWan;
 
 void recv_callback(uint8_t* message, int length){
 	Serial.println((char*)message);
@@ -9,15 +10,17 @@ void recv_callback(uint8_t* message, int length){
 
 void setup()
 {
+	// Wait for serial port to be available
 	Serial.begin(SERIAL_BAUDRATE);
-
-	while (!Serial) ; // Wait for serial port to be available
+	while (!Serial) ;
 	Serial.println("Start Sketch");
 
-	if(!LoRaWan::init(RF_FREQUENCY))
-		Serial.println("init failed");
-
-	LoRaWan::setRecvCallback(recv_callback);
+	// initialize lora wan
+	loRaWan = LoRaWan::getInstance();
+	if(!loRaWan->init(recv_callback)) {
+		Serial.println("LoRa init failed");
+		exit(0);
+	}
 }
 
 /*
@@ -26,15 +29,15 @@ void setup()
  */
 void loop()
 {
-	char payload[] = "test string";
-
-	/* application stuff
+	/* application codes
+	 * 	- delay() function should not be used in main loop
+	 *
 	 * ex)
 	 * 	haveSomethingToSend = getSensorData();
 	 * 	if (haveSomethingToSend)
-	 * 		LoRaWan::requestSend((uint8_t*)payload, sizeof(payload));
+	 * 		LoRaWan::requestSend((uint8_t*)"test", sizeof("test"));
 	 */
 
 	// LoRa loop
-	LoRaWan::oneLoop();
+	loRaWan->oneLoop();
 }
